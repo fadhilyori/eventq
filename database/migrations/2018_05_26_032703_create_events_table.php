08c6a94f19bc1e44d8d5125cad6fe433022613b5
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2018-05-26 11:43:58 +0700

    Ïnitial Commit

---
+++ database/migrations/2018_05_26_032703_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('type');
+            $table->string('location');
+            $table->date('date');
+            $table->boolean('need_regis');
+            $table->string('contact_person');
+            $table->date('start_regis')->nullable();
+            $table->date('end_regis')->nullable();
+            $table->string('img_type')->nullable();
+            $table->string('img_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
